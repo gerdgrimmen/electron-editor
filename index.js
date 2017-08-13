0f7e5a5bc9dd7a280b5e1422516b499461276d2b
funki roadkill <funki@funkimob.lan>
2017-08-13 21:53:52 +0200

    currentLine edit+refresh, put tempcontent in lines

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 testbutton = document.createElement('button');
 testbutton.textContent = "Testbutton";
-var currentFile = location.pathname.substring(location.pathname.lastIndexOf("/") + 1).split('.',1);
-var jsFile = currentFile+'.js';
-var name = [];
-var bla = jsFile;
+
+testbutton.addEventListener('click', () => {edit.writeLinesToCode()});
+document.getElementById("button_area_left").appendChild(testbutton);
+/*
 testbutton.addEventListener('click', () => {
     //console.log(typeof(name));
     document.getElementById('code').innerHTML = location.pathname + "\n"  + bla;
     edit.writeLinesToCode();
 });
-document.getElementById("button_area_left").appendChild(testbutton);
+//*/
+
+plusbutton = document.createElement('button');
+plusbutton.textContent = "+";
+plusbutton.addEventListener('click', () => {edit.currentLineControl('+')});
+document.getElementById("button_area_right").appendChild(plusbutton);
+
+
+minusbutton = document.createElement('button');
+minusbutton.textContent = "-";
+minusbutton.addEventListener('click', () => {edit.currentLineControl('-')});
+document.getElementById("button_area_right").appendChild(minusbutton);
 
 
--- lib.js
+++ lib.js
@@ -2,15 +2,47 @@ var fs = require('fs');
 
 var editor = function(){
     this.lines = {};
+    this.tempcontent = {content: 'asdtemp'};
+    this.currentLine = 0;
+
     this.saveLine = function(Line){
-        
+        this.lines[Line] = this.tempcontent;
+        console.log(this.lines);
     };
+
     this.writeLinesToCode = function(){
         var stringToWrite = ''; 
         for (key in this.lines){
             stringToWrite += edit.lines[key].content + '\n';
         };
         document.getElementById('code').innerHTML = stringToWrite;
+        this.createTempContent();
+        this.saveLine(this.currentLine);
+    };
+
+    this.currentLineControl = function(mod){
+        switch (mod) {
+            case '+':
+                this.currentLine += 1;
+                break;
+            case '-':
+                this.currentLine -= 1;
+                
+                break;
+        
+            default:
+                break;
+        }
+        this.refreshLine();
+    };
+
+    this.refreshLine = function() {
+        document.getElementById('currentLine').innerHTML = this.currentLine;
+    };
+
+    this.createTempContent = function(){
+        this.tempcontent = {content: document.getElementById('input').value};
+        console.log(this.tempcontent);
     };
 };
 
@@ -25,12 +57,14 @@ function myrequire(){
         console.log(jsFile+' does not exist! This can usually be ignored.')
     }
 };
-
+//*
 var edit = new editor();
 edit.lines[0]={content:'asd0'};
 edit.lines[1]={content:'asd1'};
 edit.lines[4]={content:'asd4'};
 edit.lines[5]={content:'asd5'};
+edit.saveLine(9);
+//*/
 //*
 console.log(typeof(edit.lines));
 console.log(typeof(edit.saveLine));
