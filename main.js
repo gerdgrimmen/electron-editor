735258302a538e87c1ddd3ee2fcbc2bb8b3698de
funki roadkill <funki@funkimob.lan>
2017-08-13 11:14:20 +0200

    first working init

---
+++ main.js
@@ -0,0 +1,8 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+app.on('ready',() =>{
+    let win = new BrowserWindow({width: 500, height:380});
+    win.loadURL('file://'+__dirname+'/index.html');
+    // win.webContents.openDevTools();
+})
